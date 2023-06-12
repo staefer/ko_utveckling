057c4880315a64d0bab813b36fcb8f5f17f7f241
Jonas Carlsson <jonas.carlsson@sogeti.com>
2023-06-12 19:20:28 +0200

    Jonas Test

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import {
+	"fmt"
+	"../test.go"
+}
 
 func main() {
 	fmt.Println("Hello KO-utvecklare!!!")
@@ -8,8 +11,3 @@ func main() {
 }
 
 
-func printLoremIpsum() {
-	loremIpsum := `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-
-	fmt.Println(loremIpsum)
-}
